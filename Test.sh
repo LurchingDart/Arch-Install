f41e58f3bf06f2a1111fd73166d3be9e0d3af4ac
LurchingDart <luizalferez25@gmail.com>
2023-04-25 23:14:21 -0600

    Change patron variable

--- Test.sh
+++ Test.sh
@@ -1,33 +1,31 @@
 mostrar_animacion_carga() {
-    local -r ancho_campo=10
-    local -r ancho_asterisco=1
-    local -r ancho_espacio=$((ancho_campo - ancho_asterisco))
+  local -r ancho_campo=10
+  local -r ancho_asterisco=1
+  local -r ancho_espacio=$((ancho_campo - ancho_asterisco))
 
-    local -r patron_asterisco1="*"
-    local -r patron_asterisco2=" *⁕※⁜⁂°º⌀⏒⌂⨀⨁⨂⨳⨶⨷⨸ "
-    local -r patron_asterisco3="*"
-    local -r patron_espacio=$(printf "%${ancho_espacio}s")
+  local -r patron_asterisco="* * *"
+  local -r patron_espacio=$(printf "%${ancho_espacio}s")
 
-    local -r patron="$patron_espacio${patron_asterisco1}${patron_asterisco2}${patron_asterisco3}$patron_espacio"
+  local -r patron="$patron_espacio${patron_asterisco}$patron_espacio"
 
-    local -r longitud_patron=${#patron}
-    local -i posicion=0
-    local direccion=1
+  local -r longitud_patron=${#patron}
+  local -i posicion=0
+  local direccion=1
 
-    local animando=true
+  local animando=true
 
-    while [ $animando == true ]; do
-        printf "\033[0;34m\r[\033[0m\033[1;93m%s\033[0m \033[0;34m]\033[0m" "${patron:posicion:ancho_campo}"
-        posicion+=direccion
+  while [ $animando == true ]; do
+    printf "\033[0;34m\r[\033[0m\033[1;93m%s\033[0m \033[0;34m]\033[0m" "${patron:posicion:ancho_campo}"
+    posicion+=direccion
 
-        if [ $posicion -lt 0 ]; then
-        direccion=1
-        elif [ $posicion -ge $((longitud_patron - ancho_campo)) ]; then
-        direccion=-1
-        fi
+    if [ $posicion -lt 0 ]; then
+      direccion=1
+    elif [ $posicion -ge $((longitud_patron - ancho_campo)) ]; then
+      direccion=-1
+    fi
 
     sleep 0.1
-    done
+  done
 }
 
 detener_animacion_carga() {
@@ -40,6 +38,6 @@ printf "\033[0;31m Hola \033[0m\n\n"
 mostrar_animacion_carga &
 PID=$!
 # Realizar tarea que toma mucho tiempo
-sleep 5
+sleep 3
 detener_animacion_carga
 printf "\r\033[2KProceso completado\n"  # borrar la última línea impresa y agregar mensaje
